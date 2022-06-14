5af77cee5ea042a51ff52853c0c86e2bcb34e1b7
tonsz <tmestrera@gmail.com>
2022-06-14 11:31:57 +0800

    Voter Log In Page

---
+++ voter_log.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="pages-style.css">
+    <title>Online Election System | Home</title>
+</head>
+<body>
+
+       <div class="top">
+           <div class="title">
+               Online Election System
+           </div>     
+           <div class="start">                                                                    
+               <a href="#"class="log-in">Log In</a> 
+               <a href="#" class="reg">Register</a>                                         
+          </div>
+        </div>
+        <div class="main-nav">
+             <ul class="menu">
+                  <li>
+                       <a href="home.php">Home</a>
+                  </li>
+                  <li >
+                       <a id="admin-log" href="admin_log.php">Administrator</a>
+                  </li>
+                  <li>
+                       <a id="voter-log" href="voter_log.php">Voter</a>
+                  </li>
+                  <li>
+                       <a href="#">Help</a>
+                  </li>
+                  <li>
+                       <a href="#">Contact Us</a>
+                  </li>
+             </ul>
+        </div>
+
+        <!-- dito ka siz start sa code mo  -->
+        <h1> hello </h1>
+     
+
+     <!-- para mahighlight ung page sa navigation bar -->
+     <style>
+          #voter-log {
+               color: #1b2459;
+               border: 10px solid transparent;
+               background-color: #e2e3eb;
+               border-color: #e2e3eb;
+              
+          }
+     </style>
+        
+    
+</body>
+</html>
