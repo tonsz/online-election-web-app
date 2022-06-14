17431486728d30c84559804181b7f55e4e42d16c
tonsz <tmestrera@gmail.com>
2022-06-14 11:31:07 +0800

    Home Page

---
+++ home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home-style.css">
+    <title>Online Election System | Home</title>
+</head>
+<body>
+
+       <div class="top">
+           <div class="title">
+               Online Election System
+           </div>     
+           <div class="start">                                                                    
+               <a href="#"class="log-in">Log In</a> 
+               <a href="#" class="reg">Register</a>                                         
+          </div>
+        </div>
+        <div class="main-nav">
+             <ul class="menu">
+                  <li>
+                       <a href="home.php">Home</a>
+                  </li>
+                  <li>
+                       <a href="admin_log.php">Administrator</a>
+                  </li>
+                  <li>
+                       <a href="voter_log.php">Voter</a>
+                  </li>
+                  <li>
+                       <a href="#">Help</a>
+                  </li>
+                  <li>
+                       <a href="#">Contact Us</a>
+                  </li>
+             </ul>
+        </div>
+     
+        
+    
+</body>
+</html>
