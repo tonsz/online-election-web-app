7f40cec9c20a85a6a0b5d3ce206830a37fa35f32
tonsz <tmestrera@gmail.com>
2022-06-16 11:07:07 +0800

    Manage Elections Page

---
+++ manage-election.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="adminpages-style.css">
+    <title>Online Election System | Admin </title>
+</head>
+<body>
+
+       <div class="top">
+           <div class="title">
+               Online Election System
+           </div>     
+           <div class="start">                                                                    
+               <h2 href="#" class="greet">Hello, Someone</h2>                                         
+          </div>
+        </div>
+        <div class="main-nav">
+             <ul class="menu">
+                  <li>
+                       <a href="admin-home.php">Home</a>
+                  </li>
+                  <li>
+                       <a href="manage-election.php" id="manage-elec">Manage Election</a>
+                  </li>
+                  <li>
+                       <a href="result.php">View Results</a>
+                  </li>
+                  <li>
+                       <a id="help" href="help.php">Help</a>
+                  </li>
+                  <li>
+                       <a id="contact" href="contact.php">Contact Us</a>
+                  </li>
+             </ul>
+        </div>
+
+        <!-- code mo sis here -->
+     
+         <!-- para mahighlight ung page sa navigation bar -->
+     <style>
+          #manage-elec {
+               color: #1b2459;
+               border: 10px solid transparent;
+               background-color: #e2e3eb;
+               border-color: #e2e3eb;
+              
+          }
+     </style>
+        
+    
+</body>
+</html>
+
