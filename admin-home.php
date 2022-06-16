6746bab95e4acb6d7424cad5c825bf374522e464
tonsz <tmestrera@gmail.com>
2022-06-16 11:01:28 +0800

    Admin Home Page

---
+++ admin-home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="adminhome-style.css">
+    <title>Online Election System | Admin </title>
+</head>
+<body>
+
+       <div class="top">
+           <div class="title">
+               Online Election System
+           </div>     
+           <div class="start">                                                                    
+               <h2 href="#" class="greet">Hello, Someone</h2>                                         
+          </div>
+        </div>
+        <div class="main-nav">
+             <ul class="menu">
+                  <li>
+                       <a href="admin-home.php">Home</a>
+                  </li>
+                  <li>
+                       <a href="manage-election.php">Manage Election</a>
+                  </li>
+                  <li>
+                       <a href="result.php">View Results</a>
+                  </li>
+                  <li>
+                       <a id="help" href="help.php">Help</a>
+                  </li>
+                  <li>
+                       <a id="contact" href="contact.php">Contact Us</a>
+                  </li>
+             </ul>
+        </div>
+
+        <div class="container">
+               <h2 class="vote">
+                    Every vote counts.
+               </h2>
+               <p> The Online Election System is a platform for institutions to conduct their own elections or voting event polls. Register, know your election ID and let your choice count. </p>
+               <a href="new-election.php" class="create-btn">CREATE ELECTION</a>
+        </div>
+     
+        
+    
+</body>
+</html>
+
